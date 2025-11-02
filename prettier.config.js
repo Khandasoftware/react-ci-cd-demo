8049e8a253ba696346b18a15acf433c4f96ad8aa
Khandasoftware <khandasoftware@gmail.com>
2025-11-02 18:51:23 +0200

    Configs for formatting

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+/** @type {import('prettier').Options} */
+export default {
+  semi: false, // no semicolons
+  bracketSameLine: false, // keep closing brackets on a new line (mostly for JSX)
+  singleQuote: true, // optional: single quotes
+  trailingComma: 'none', // optional: remove trailing commas
+  endOfLine: 'auto' // optional: line endings
+}
